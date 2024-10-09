642bed57b82c2d450d56febe312c2a93344af50a
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2024-10-09 16:58:21 +0300

    for-cycles-done

---
+++ index.js
@@ -0,0 +1,40 @@
+// 1 start.
+
+// 1 end.
+
+// 2 start.
+// for (let u = 2; u <= 20; u = u + 1) {
+//     if (u % 2 === 0) {
+//         console.log(u)
+//     }
+// }
+
+
+// 2 end.
+
+// 3start
+for (let j = 7; j <= 70; j = j + 7) {
+    console.log(j)
+}
+// 3 end.
+
+// 4 start.
+
+// 4 end.
+
+// 5 start.
+for (let x = 1; x <= 10; x = x + 1) {
+    if (x > 7 || x === 7) {
+        break;
+    }
+    console.log(x)
+}
+// 5 end.
+
+// 6 start.
+
+// 6 end.
+
+// 7 start.
+
+// 7 end.
