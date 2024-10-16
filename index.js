ce2dbcccf4f8241f297f04da2153878338a86183
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2024-10-16 15:19:19 +0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // 1 start.
+let b = 1;
+
+while (b <= 10) {
+    console.log(b)
+    b = b + 1
+}
+
 
 // 1 end.
 
 // 2 start.
-// for (let u = 2; u <= 20; u = u + 1) {
-//     if (u % 2 === 0) {
-//         console.log(u)
-//     }
-// }
+for (let u = 2; u <= 20; u++) {
+    if (u % 2 !== 0) {
+        continue;
+    }
+    console.log(u)
+}
 
 
 // 2 end.
@@ -19,22 +27,49 @@ for (let j = 7; j <= 70; j = j + 7) {
 // 3 end.
 
 // 4 start.
+let massive = [1, 2, 3, 4, 5];
+let i = 0;
 
+while (i < massive.length) {
+    console.log(massive[i]);
+    i++;
+}
 // 4 end.
 
 // 5 start.
-for (let x = 1; x <= 10; x = x + 1) {
-    if (x > 7 || x === 7) {
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let m = 0; m < numbers.length; m++) {
+    if (numbers[m] === 7) {
         break;
     }
-    console.log(x)
+    console.log(numbers[m]);
 }
 // 5 end.
 
 // 6 start.
+let numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let n = 9;
+
+for (let c = 0; c < numbers1.length; c++) {
+    if (numbers1[c] >= n) {
+        break;
+    }
+    console.log(numbers1[c]);
+}
 
 // 6 end.
 
 // 7 start.
+let num = 1;
+
+while (num <= 20) {
+    if (num % 3 === 0) {
+        num++;
+        continue;
+    }
+    console.log(num);
+    num++;
+}
 
 // 7 end.
